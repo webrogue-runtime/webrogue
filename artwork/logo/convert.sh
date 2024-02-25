b5975fd61dc242502c7d56eaa05f4d6b4fa373e7
Artem Borovik <polzovatellllk@gmail.com>
2024-02-26 02:09:18 +0300

    more compact logo convert script

--- artwork/logo/convert.sh
+++ artwork/logo/convert.sh
@@ -6,13 +6,9 @@ svgo logo.svg -o logo_optimized.svg
 # sudo apt install librsvg2-bin
 rsvg-convert -w 1024 -h 1024 logo.svg -o logo_black_transparent_1024.png
 
-black_white_png() {
-    rsvg-convert -w $1 -h $1 --background-color white logo.svg -o logo_black_white_$1.png
-}
 
 android_black_white() {
-    black_white_png $1
-    mv logo_black_white_$1.png ../../platforms/Android/app/src/main/res/mipmap-$2/ic_launcher.png
+    rsvg-convert -w $1 -h $1 --background-color white logo.svg -o ../../platforms/Android/app/src/main/res/mipmap-$2/ic_launcher.png
 }
 android_black_white 48 mdpi
 android_black_white 72 hdpi
