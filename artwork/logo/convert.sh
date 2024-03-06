4ffcb7023b019812d17b2f2d07c7438ae38ffa2a
Artem Borovik <polzovatellllk@gmail.com>
2024-03-06 13:49:46 +0300

    better logos

--- artwork/logo/convert.sh
+++ artwork/logo/convert.sh
@@ -1,12 +1,11 @@
 cd $(dirname $0)
 
 set -ex
+
 # sudo snap install svgo
-svgo logo.svg -o logo_optimized.svg
 # sudo apt install librsvg2-bin
-rsvg-convert -w 1024 -h 1024 logo.svg -o logo_black_transparent_1024.png
 
-
+# For Android
 android_black_white() {
     rsvg-convert -w $1 -h $1 --background-color white logo.svg -o ../../platforms/Android/app/src/main/res/mipmap-$2/ic_launcher.png
 }
@@ -15,8 +14,11 @@ android_black_white 72 hdpi
 android_black_white 96 xhdpi
 android_black_white 144 xxhdpi
 android_black_white 192 xxxhdpi
+rsvg-convert -w 512 -h 512 logo.svg -o ../../platforms/Android/app/src/main/res/drawable/ic_launcher_black_foreground.png
 
-rsvg-convert -w 512 -h 512 logo.svg -o logo_black_transparent_512.png
-mv logo_black_transparent_512.png ../../platforms/Android/app/src/main/res/drawable/ic_launcher_black_foreground.png
+# For Linux
+rsvg-convert -w 256 -h 256 logo.svg -o ../../platforms/Linux/TemplateAppDir/.DirIcon
+svgo logo.svg -o ../../platforms/Linux/TemplateAppDir/Webrogue.svg
 
-convert -background transparent -define 'icon:auto-resize=16,24,32,64' logo.svg logo.ico
+# For Web
+convert -background transparent -define 'icon:auto-resize=16,24,32,64' logo.svg ../../platforms/Web/logo.ico
