5e17f0496fa312cbf7d4a4d66d3c0f7bdc4d198c
Artem Borovik <polzovatellllk@gmail.com>
2024-03-06 14:56:43 +0300

    bigger icon

--- artwork/logo/convert.sh
+++ artwork/logo/convert.sh
@@ -8,12 +8,21 @@ set -ex
 original() {
     cat logo.svg
 }
+bigger() {
+    sed "s/viewBox=\"0 0 48 48\"/viewBox=\"6 6 36 36\"/g"
+}
 
 to_png_transparent() {
     rsvg-convert -w $1 -h $1 /dev/stdin -o $2
 }
 to_png_white() {
     rsvg-convert -w $1 -h $1 --background-color white /dev/stdin -o $2
+}
+to_svg() {
+    svgo /dev/stdin -o $1
+}
+to_ico() {
+    convert -background transparent -define 'icon:auto-resize=16,24,32,64' /dev/stdin $1
 }
 
 # For Android
@@ -29,7 +38,7 @@ original | to_png_transparent 512 ../../platforms/Android/app/src/main/res/drawa
 
 # For Linux
 original | to_png_transparent 256 ../../platforms/Linux/TemplateAppDir/.DirIcon
-svgo logo.svg -o ../../platforms/Linux/TemplateAppDir/Webrogue.svg
+original | to_svg ../../platforms/Linux/TemplateAppDir/Webrogue.svg
 
 # For Web
-original | convert -background transparent -define 'icon:auto-resize=16,24,32,64' /dev/stdin ../../platforms/Web/logo.ico
+original | bigger | to_ico ../../platforms/Web/logo.ico
