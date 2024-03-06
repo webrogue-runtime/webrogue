9483e6f780f1e54195d057ab53dd1ecc81a950da
Artem Borovik <polzovatellllk@gmail.com>
2024-03-06 14:47:43 +0300

    logo color fix

--- artwork/logo/convert.sh
+++ artwork/logo/convert.sh
@@ -5,20 +5,31 @@ set -ex
 # sudo snap install svgo
 # sudo apt install librsvg2-bin
 
-# For Android
-android_black_white() {
-    rsvg-convert -w $1 -h $1 --background-color white logo.svg -o ../../platforms/Android/app/src/main/res/mipmap-$2/ic_launcher.png
+original() {
+    cat logo.svg
 }
-android_black_white 48 mdpi
-android_black_white 72 hdpi
-android_black_white 96 xhdpi
-android_black_white 144 xxhdpi
-android_black_white 192 xxxhdpi
-rsvg-convert -w 512 -h 512 logo.svg -o ../../platforms/Android/app/src/main/res/drawable/ic_launcher_black_foreground.png
+
+to_png_transparent() {
+    rsvg-convert -w $1 -h $1 /dev/stdin -o $2
+}
+to_png_white() {
+    rsvg-convert -w $1 -h $1 --background-color white /dev/stdin -o $2
+}
+
+# For Android
+android_old() {
+    original | to_png_white $1 ../../platforms/Android/app/src/main/res/mipmap-$2/ic_launcher.png
+}
+android_old 48 mdpi
+android_old 72 hdpi
+android_old 96 xhdpi
+android_old 144 xxhdpi
+android_old 192 xxxhdpi
+original | to_png_transparent 512 ../../platforms/Android/app/src/main/res/drawable/ic_launcher_black_foreground.png
 
 # For Linux
-rsvg-convert -w 256 -h 256 logo.svg -o ../../platforms/Linux/TemplateAppDir/.DirIcon
+original | to_png_transparent 256 ../../platforms/Linux/TemplateAppDir/.DirIcon
 svgo logo.svg -o ../../platforms/Linux/TemplateAppDir/Webrogue.svg
 
 # For Web
-convert -background transparent -define 'icon:auto-resize=16,24,32,64' logo.svg ../../platforms/Web/logo.ico
+original | convert -background transparent -define 'icon:auto-resize=16,24,32,64' /dev/stdin ../../platforms/Web/logo.ico
