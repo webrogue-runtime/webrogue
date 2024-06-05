28c0a4c648963089cef9256a9e1c528f1f003ef6
Artem Borovik <polzovatellllk@gmail.com>
2024-06-05 23:37:54 +0300

    update

--- platforms/Linux/ubuntu.sh
+++ platforms/Linux/ubuntu.sh
@@ -6,6 +6,6 @@ image_name=webrogue_deb
 git rev-parse --verify HEAD > git_hash
 docker build -t $image_name -f ubuntu.Dockerfile .
 id=$(docker create $image_name)
-docker cp $id:webrogue.deb - | tar -x
-docker cp $id:webrogue.rpm - | tar -x
+docker cp $id:webrogue.deb webrogue.deb
+docker cp $id:webrogue.rpm webrogue.rpm
 docker rm -v $id
