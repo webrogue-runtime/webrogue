7e73a243bf1ecff947c2fe03245325b169757423
Artem Borovik <polzovatellllk@gmail.com>
2024-06-05 23:56:27 +0300

    fix

--- platforms/Linux/deb2appimage.sh
+++ platforms/Linux/deb2appimage.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)
 image_name=deb2appimage
 docker build -t $image_name -f deb2appimage.Dockerfile .
 id=$(docker create $image_name)
-docker cp $id:Webrogue-x86_64.AppImage webrogue-x86_64.AppImage
+docker cp $id:webrogue-x86_64.AppImage webrogue-x86_64.AppImage
 docker rm -v $id
