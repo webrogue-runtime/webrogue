08b3e4c46346cbfcd0172af0b9d2ff9056780341
Artem Borovik <polzovatellllk@gmail.com>
2024-11-14 14:27:42 +0300

    apple support

--- apple/macOS/runtime/rust/src/lib.rs
+++ apple/macOS/runtime/rust/src/lib.rs
@@ -1,49 +1,6 @@
-use webrogue_runtime::WasiFactory;
-
-fn make_backend() -> webrogue_backend_wasmtime::Backend {
-    webrogue_backend_wasmtime::Backend::new()
-}
-use webrogue_backend_wasmtime::make_funcs;
-
-make_funcs!({
-    "wasi_snapshot_preview1": {
-        module: webrogue_wasi::wasi_snapshot_preview1
-    },
-    "webrogue_gl": {
-        module: webrogue_gl::api
-    },
-    "webrogue_gfx": {
-        module: webrogue_gfx
-    }
-});
-
 fn main(wrapp_path: String) -> anyhow::Result<()> {
-    let lifecycle = webrogue_runtime::Lifecycle::new();
-
-    let wasi_factory = webrogue_wasi_sync::WasiFactory::new();
-    let mut wasi = wasi_factory.make();
-
-    wasi_factory.add_actual_dir(&mut wasi, std::env::current_dir()?, "/");
-
-    webrogue_std_stream_os::bind_streams(&mut wasi);
-    let backend = make_backend();
-
-    let reader = webrogue_runtime::wrapp::WrappHandle::from_file_path(std::path::PathBuf::from(wrapp_path))?;
-    let mut webrogue_gfx_context = webrogue_gfx::Context::new(Box::new(webrogue_gfx_ffi::make_system));
-    let mut webrogue_gl_context = webrogue_gl::api::Context::new(
-        &mut webrogue_gfx_context,
-    );
-    lifecycle.run(
-        backend,
-        make_imports(),
-        make_context_vec(
-            &mut wasi,
-            &mut webrogue_gl_context,
-            &mut webrogue_gfx_context,
-        ),
-        reader,
-    )?;
-
+    let handle = webrogue_runtime::wrapp::WrappHandle::from_file_path(std::path::PathBuf::from(wrapp_path))?;
+    webrogue_runtime::run(handle, None)?;
     Ok(())
 }
 
