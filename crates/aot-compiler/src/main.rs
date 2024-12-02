b01bd937ec8d8ecf8c61572c9979ed30ef2c6043
Artem Borovik <polzovatellllk@gmail.com>
2024-12-02 20:04:11 +0300

    help for webrogue-aot-compiler cli

--- crates/aot-compiler/src/main.rs
+++ crates/aot-compiler/src/main.rs
@@ -4,6 +4,7 @@ mod compile;
 mod linux;
 mod windows_mingw;
 
+/// webrogue-aot-compiler builds native applications from WebC files
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 // #[command(propagate_version = true)]
@@ -11,39 +12,52 @@ struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
-
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Build Linux executable
+    Linux {
+        /// Path to WebC
+        webc_path: std::path::PathBuf,
+        /// Path where resulting executable will be placed
+        out_path: std::path::PathBuf,
+    },
+    /// Android-related commands
+    Android {
+        #[command(subcommand)]
+        commands: AndroidCommands,
+    },
+    /// Windows-related commands
+    Windows {
+        #[command(subcommand)]
+        commands: WindowsCommands,
+    },
+    /// Compile object file.
+    /// This commands is intended be invoked from other build systems
     Object {
         webc_path: std::path::PathBuf,
         out_path: std::path::PathBuf,
         target: String,
     },
-    Linux {
-        webc_path: std::path::PathBuf,
-        out_path: std::path::PathBuf,
-    },
-    Android {
-        #[command(subcommand)]
-        commands: AndroidCommands,
-    },
-    Windows {
-        #[command(subcommand)]
-        commands: WindowsCommands,
-    },
 }
 #[derive(Subcommand, Debug)]
 enum AndroidCommands {
+    /// Make Gradle Android project
     Gradle {
+        /// Path to Android SDK
         sdk_path: std::path::PathBuf,
+        /// Path to WebC
         webc_path: std::path::PathBuf,
+        /// Path where resulting project will be placed
         build_dir: std::path::PathBuf,
     },
 }
 #[derive(Subcommand, Debug)]
 enum WindowsCommands {
-    MinGW {
+    /// Build Windows executable using MinGW
+    Mingw {
+        /// Path to WebC
         webc_path: std::path::PathBuf,
+        /// Path where resulting executable will be placed
         out_path: std::path::PathBuf,
     },
 }
@@ -74,7 +88,7 @@ fn main() -> anyhow::Result<()> {
             }
         },
         Commands::Windows { commands } => match commands {
-            WindowsCommands::MinGW {
+            WindowsCommands::Mingw {
                 webc_path,
                 out_path,
             } => windows_mingw::build_windows_mingw(webc_path, out_path)?,
