8d4db3ff28570f7249d62a965926f17028627533
Artem Borovik <polzovatellllk@gmail.com>
2024-12-21 02:06:44 +0300

    update linux aot artifacts build scripts

--- linux/build_linux_aot_artifacts.sh
+++ linux/build_linux_aot_artifacts.sh
@@ -1,4 +1,4 @@
-cd $(dirname $0)
+cd $(dirname $0)/docker
 set -ex
 
 IMAGE_NAME=webrogue/webrogue-linux-aot-artifacts-builder
@@ -9,7 +9,7 @@ docker run \
     -v "$(dirname $(dirname $PWD))":/usr/src/myapp \
     -w /usr/src/myapp \
     $IMAGE_NAME \
-    sh scripts/_build_linux_aot_artifacts.sh \
+    sh linux/_build_linux_aot_artifacts.sh
 # || docker run \
 #     -it \
 #     --rm \
