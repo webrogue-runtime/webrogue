ce5ab0700bd33ab20a8c740225c2cc9931fe85f6
Artem Borovik <polzovatellllk@gmail.com>
2025-04-29 22:52:55 +0300

    fixes

--- artwork/logo/convert_using_docker.sh
+++ artwork/logo/convert_using_docker.sh
@@ -2,10 +2,13 @@ set -ex
 
 cd $(dirname $0)
 
+docker --version | grep podman >/dev/null || {
+    DOCKER_USER_FLAGS="-u $(id -u ${USER}):$(id -g ${USER})"
+}
 image_name=webrogue_logo_converter
 docker build -t $image_name -f convert.Dockerfile .
 echo $(dirname $(dirname $(pwd)))
 docker run \
     -v $(dirname $(dirname $(pwd))):/host_dir \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
+    $DOCKER_USER_FLAGS \
     -t $image_name
--- linux/glibc/build_cli.sh
+++ linux/glibc/build_cli.sh
@@ -4,4 +4,4 @@ set -ex
 export NUM_JOBS=$(nproc)
 
 export OPENSSL_STATIC=1
-cargo build --target-dir=./target --target=x86_64-unknown-linux-gnu --features=compile --profile cli
+cargo build --target-dir=./target --target=x86_64-unknown-linux-gnu --features=full --profile cli
