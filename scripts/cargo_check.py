29876c642d25f47ca044ceeac6f85371ce908d75
Artem Borovik <polzovatellllk@gmail.com>
2026-06-11 15:28:54 +0300

    fix

--- scripts/cargo_check.py
+++ scripts/cargo_check.py
@@ -12,6 +12,17 @@ except ImportError:
 
 repo_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+components = set(os.getenv("COMPONENTS", "all").split(","))
+
+def is_component_selected(component):
+    if "all" in components:
+        return True
+    try:
+        components.remove(component)
+        return True
+    except KeyError:
+        return False
+
 pending_checks = []
 
 def check(package=None, target=None, features=None, ndk_target=None):
@@ -43,33 +54,40 @@ def get_all_subsets(arr):
             subsets.append(list(comp))
     return subsets
 
-targets = []
+native_targets = []
 if sys.platform == "win32":
-    targets.append("x86_64-pc-windows-msvc")
+    native_targets.append("x86_64-pc-windows-msvc")
 elif sys.platform == "darwin":
-    targets.extend(["x86_64-apple-darwin", "aarch64-apple-darwin"])
+    native_targets.extend(["x86_64-apple-darwin", "aarch64-apple-darwin"])
 elif sys.platform == "linux":
-    targets.extend(["x86_64-unknown-linux-gnu"])
+    # Omit --target options for native Linux builds
+    native_targets.append(None)
 else:
     raise Exception(f"Unsupported platform: {sys.platform}")
 
+if is_component_selected("cli"):
+    for target in native_targets:
+        check(package="webrogue", target=target)
+        for features in get_all_subsets(["run", "compile", "pack", "hub", "llvm"]):
+            check(package="webrogue", target=target, features=features)
 
-for target in targets:
-    check(package="webrogue", target=target)
-    for features in get_all_subsets(["run", "compile", "pack", "hub", "llvm"]):
-        check(package="webrogue", target=target, features=features)
+if is_component_selected("aot-lib"):
+    for target in native_targets:
+        check(package="webrogue-aot-lib", target=target)
+        for gfxstream_type in ["impl", "stub"]:
+            check(package="webrogue-gfxstream-lib", target=target, features=[gfxstream_type])
 
-for target in targets:
-    check(package="webrogue-aot-lib", target=target)
-    for gfxstream_type in ["impl", "stub"]:
-        check(package="webrogue-gfxstream-lib", target=target, features=[gfxstream_type])
+if is_component_selected("android"):
+    for ndk_target in ["arm64-v8a", "x86_64"]:
+        for features in [["launcher"], ["runner"]]:
+            check(package="webrogue-android", ndk_target=ndk_target, features=features)
 
-for ndk_target in ["arm64-v8a", "x86_64"]:
-    for features in [["launcher"], ["runner"]]:
-        check(package="webrogue-android", ndk_target=ndk_target, features=features)
-
-# for features in [["launcher"], ["runner"]]:
-#     check(package="webrogue-ios", features=features)
+# if is_component_selected("ios"):
+#     for features in [["launcher"], ["runner"]]:
+#         check(package="webrogue-ios", features=features)
 
 for pending_check in tqdm_func(pending_checks):
     subprocess.run(pending_check, cwd=repo_dir, check=True)
+
+if components and components != set(["all"]):
+    raise Exception(f"Some specified components were not checked: {components}")
