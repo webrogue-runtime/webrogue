314b2367cd43b3193919990c0b1bf580e5d8c408
Artem Borovik <60874647+Destructor17@users.noreply.github.com>
2026-06-12 11:00:46 +0300

    Cargo checks on CI (#68)

--- apple/runtime/ios/rust/src/launcher.rs
+++ apple/runtime/ios/rust/src/launcher.rs
@@ -2,8 +2,14 @@ use webrogue_launcher::App;
 use winit::event_loop::EventLoop;
 
 #[no_mangle]
-pub unsafe extern "C" fn webrogue_ios_rs_main_launcher() {
+pub unsafe extern "C" fn webrogue_ios_rs_main_launcher(persistent_path: *const i8) {
     let event_loop = EventLoop::new().unwrap();
-    let mut app = App::new(false);
-    event_loop.run_app(&mut app).unwrap();
+
+    let persistent_path = std::ffi::CStr::from_ptr(persistent_path as *const _)
+        .to_str()
+        .unwrap()
+        .to_owned();
+
+    let app = App::new(false, std::path::PathBuf::from(persistent_path));
+    event_loop.run_app(app).unwrap();
 }
